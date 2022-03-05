b0998ea8c486e79a18a2ec0f610ab1048acedd62
Ashish <ashishprakash52433@gmail.com>
2022-03-05 13:11:36 +0530

    Destructuring and Arrow

---
+++ ES6/arrowfn.js
@@ -0,0 +1,8 @@
+let fun = x => 2*x;
+
+console.log(fun(5));;
+
+let fa = (x,y) => {
+    return x+y;
+}
+console.log(fa(2,3));
---
+++ ES6/destructuring.js
@@ -0,0 +1,14 @@
+let address = {
+    city:"Calcutta",
+    pincode:110086,
+    region:"Habra"
+}
+
+let {region, pincode, city} = address;
+
+console.log(region);
+console.log(city);
+console.log(pincode);
+
+
+
